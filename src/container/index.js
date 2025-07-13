baeaabc8a0094fcc93f8e70f20b1f3d701bc7570
Apple <1234@MacBook-Pro.local>
2025-07-13 00:54:58 -0700

    push

--- src/container/index.js
+++ src/container/index.js
@@ -1,7 +1,7 @@
 
 export { default as AboutUs } from './AboutUs/AboutUs';
 export { default as Chef } from './Chef/Chef';
-export { default as FindUs } from './FindUs/FindUs';
+export { default as FindUs } from './Findus/FindUs';
 export { default as Footer } from './Footer/Footer';
 export { default as Gallery } from './Gallery/Gallery';
 export { default as Header } from './Header/Header';
